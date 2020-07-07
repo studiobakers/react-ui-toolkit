98f2511a08791f2127b5c2e0e3ef3a4c8e6432e1
Mucahit Tutuncu <tutuncumucahit@gmail.com>
2020-07-07 16:55:22 +0300

    chore(tab): Create TabHeaderItem component

---
+++ src/tab/header/item/TabHeaderItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import {TabItem} from "../../Tab";
+import Button from "../../../button/Button";
+
+type TabHeaderItemProps = Omit<TabItem, "id"> & {
+  index: number;
+  onClick: (index: number) => void;
+};
+
+function TabHeaderItem({children, icon, index, onClick}: TabHeaderItemProps) {
+  return (
+    <Button customClassName={"tab-header-item"} onClick={handleClick}>
+      {icon && <span className={"tab-header-item-icon"}>{icon}</span>}
+
+      {children}
+    </Button>
+  );
+
+  function handleClick() {
+    onClick(index);
+  }
+}
+
+export default TabHeaderItem;
