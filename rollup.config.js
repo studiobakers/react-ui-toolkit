cc7a0221dd5bba03c4f5a06644305b59be2c3ddc
Yasin Çalışkan <yasincaliskandev@gmail.com>
2021-04-28 10:29:47 +0300

    fix(root): Add textarea dependency

--- rollup.config.js
+++ rollup.config.js
@@ -28,7 +28,8 @@ export default [
       FileUploadButton: "src/button/file-upload/FileUploadButton.tsx",
       Spinner: "src/spinner/Spinner.tsx",
       Countdown: "src/countdown/Countdown.tsx",
-      ProgressBar: "src/progress-bar/ProgressBar.tsx"
+      ProgressBar: "src/progress-bar/ProgressBar.tsx",
+      Textarea: "src/form/textarea/Textarea.tsx"
     },
     output: {
       dir: "dist",
