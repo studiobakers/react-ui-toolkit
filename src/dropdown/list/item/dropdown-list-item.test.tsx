c6a237caad63f5cfa369be0b545d15935958246e
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-07-14 17:11:26 +0300

    fix: Remove snapshot tests

--- src/dropdown/list/item/dropdown-list-item.test.tsx
+++ src/dropdown/list/item/dropdown-list-item.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {render, screen, fireEvent} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import {create} from "react-test-renderer";
 import userEvent from "@testing-library/user-event";
 
 import {testA11y} from "../../../core/utils/test/testUtils";
@@ -19,12 +18,6 @@ describe("<DropdownListItem />", () => {
 
   it("should render correctly", () => {
     render(<DropdownListItem {...defaultDropdownListItemProps} />);
-  });
-
-  it("should match snapshot", () => {
-    const tree = create(<DropdownListItem {...defaultDropdownListItemProps} />).toJSON();
-
-    expect(tree).toMatchSnapshot();
   });
 
   it("should pass a11y test", async () => {
