1594b6cecb6ce1b9427cde2c42ab4eca95cb9a2b
Mehmet Faruk Koç <mfarukkoc@gmail.com>
2021-07-14 15:09:22 +0300

    refactor(cardHeader): Change conditional block
    * Use ternary operator instead of if block
    * Move handleKeyDown function definiton below the return statement.

--- src/card/header/CardHeader.tsx
+++ src/card/header/CardHeader.tsx
@@ -4,12 +4,25 @@ import React from "react";
 import classNames from "classnames";
 
 export interface CardHeaderProps {
-  children?: React.ReactNode;
+  children: React.ReactNode;
   customClassName?: string;
   onClick?: (event?: React.MouseEvent) => void;
 }
 
 function CardHeader({children, customClassName, onClick}: CardHeaderProps) {
+  return onClick ? (
+    <div
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      role={"button"}
+      tabIndex={0}
+      className={classNames("card__header card__header--clickable", customClassName)}>
+      {children}
+    </div>
+  ) : (
+    <div className={classNames("card__header", customClassName)}>{children}</div>
+  );
+
   function handleKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
     switch (event.key) {
       case "Enter":
@@ -21,20 +34,6 @@ function CardHeader({children, customClassName, onClick}: CardHeaderProps) {
       default:
     }
   }
-  if (onClick) {
-    return (
-      <div
-        onClick={onClick}
-        onKeyDown={handleKeyDown}
-        role={"button"}
-        tabIndex={0}
-        className={classNames("card__header card__header--clickable", customClassName)}>
-        {children}
-      </div>
-    );
-  }
-
-  return <div className={classNames("card__header", customClassName)}>{children}</div>;
 }
 
 export default CardHeader;
