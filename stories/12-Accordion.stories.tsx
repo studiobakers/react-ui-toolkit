9dd3c0df4e32195d0b7a621ebb572b7c01f486fc
Mehmet Faruk Koç <mfarukkoc@gmail.com>
2021-08-02 10:33:37 +0300

    refactor(accordionStories): Update code style
    * Add empty lines between subling elements
    * Wrap props with curly brackets
    * Update single quotes with double quotes

--- stories/12-Accordion.stories.tsx
+++ stories/12-Accordion.stories.tsx
@@ -6,45 +6,49 @@ storiesOf("Accordion", module).add("Accordion", () => (
   <Fragment>
     <p>
       {
-        'Accordion with defaultExpanded={["1"]} allowZeroExpanded={false} allowMultipleExpanded={false}'
+        "Accordion with defaultExpanded={[\x221\x22]} allowZeroExpanded={false} allowMultipleExpanded={false}"
       }
     </p>
     <Accordion defaultExpanded={["1"]}>
-      <Accordion.Item accordionId="1" header="Accordion header 1">
+      <Accordion.Item accordionId={"1"} header={"Accordion header 1"}>
         Content of the Accordion 1
       </Accordion.Item>
-      <Accordion.Item accordionId="2" header="Accordion header 2">
+      <Accordion.Item accordionId={"2"} header={"Accordion header 2"}>
         Content of the Accordion 2
       </Accordion.Item>
-      <Accordion.Item accordionId="3" header="Accordion header 3">
+      <Accordion.Item accordionId={"3"} header={"Accordion header 3"}>
         Content of the Accordion 3
       </Accordion.Item>
     </Accordion>
+
     <br />
     <br />
+
     <p>{"Accordion with allowZeroExpanded={true} allowMultipleExpanded={false}"}</p>
     <Accordion allowZeroExpanded>
-      <Accordion.Item accordionId="1" header="Accordion header 1">
+      <Accordion.Item accordionId={"1"} header={"Accordion header 1"}>
         Content of the Accordion 1
       </Accordion.Item>
-      <Accordion.Item accordionId="2" header="Accordion header 2">
+      <Accordion.Item accordionId={"2"} header={"Accordion header 2"}>
         Content of the Accordion 2
       </Accordion.Item>
-      <Accordion.Item accordionId="3" header="Accordion header 3">
+      <Accordion.Item accordionId={"3"} header={"Accordion header 3"}>
         Content of the Accordion 3
       </Accordion.Item>
     </Accordion>
+
     <br />
     <br />
+
     <p>{"Accordion with allowZeroExpanded={true} allowMultipleExpanded={true}"}</p>
     <Accordion allowZeroExpanded allowMultipleExpanded>
-      <Accordion.Item accordionId="1" header="Accordion header 1">
+      <Accordion.Item accordionId={"1"} header={"Accordion header 1"}>
         Content of the Accordion 1
       </Accordion.Item>
-      <Accordion.Item accordionId="2" header="Accordion header 2">
+      <Accordion.Item accordionId={"2"} header={"Accordion header 2"}>
         Content of the Accordion 2
       </Accordion.Item>
-      <Accordion.Item accordionId="3" header="Accordion header 3">
+      <Accordion.Item accordionId={"3"} header={"Accordion header 3"}>
         Content of the Accordion 3
       </Accordion.Item>
     </Accordion>
