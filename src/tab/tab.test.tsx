86bbf05edd255e1b8651876d4abfa3970d6398c4
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-12-03 17:16:03 +0300

    feat(tab): Add tests for controlled tab component

--- src/tab/tab.test.tsx
+++ src/tab/tab.test.tsx
@@ -6,23 +6,24 @@ import {TabItem, TabProps} from "./Tab";
 import {Tab} from "..";
 import {testA11y} from "../core/utils/test/testUtils";
 
+const tabItems: TabItem[] = [
+  {
+    id: "tab-item-1",
+    content: <div data-testid={"uncontrolled-tab.content-0"}>{"Tab Item 1"}</div>
+  },
+  {
+    id: "tab-item-2",
+    content: <div data-testid={"uncontrolled-tab.content-1"}>{"Tab Item 2"}</div>
+  },
+  {
+    id: "tab-item-3",
+    content: <div data-testid={"uncontrolled-tab.content-2"}>{"Tab Item 3"}</div>,
+    isDisabled: true
+  }
+];
+
 // For uncontrolled tab component
 describe("<Tab/>", () => {
-  const tabItems: TabItem[] = [
-    {
-      id: "tab-item-1",
-      content: <div data-testid={"uncontrolled-tab.content-0"}>{"Tab Item 1"}</div>
-    },
-    {
-      id: "tab-item-2",
-      content: <div data-testid={"uncontrolled-tab.content-1"}>{"Tab Item 2"}</div>
-    },
-    {
-      id: "tab-item-3",
-      content: <div data-testid={"uncontrolled-tab.content-2"}>{"Tab Item 3"}</div>,
-      isDisabled: true
-    }
-  ];
   const defaultUncontrolledTabProps: TabProps = {
     items: tabItems,
     children: [
@@ -86,5 +87,36 @@ describe("<Tab/>", () => {
         expect(screen.getByTestId(`uncontrolled-tab.body`)).toContain(children[index]);
       }
     }
+  });
+});
+
+// For controlled tab component
+describe("<Tab/>", () => {
+  const defaultControlledTabProps: TabProps = {
+    items: tabItems,
+    children: [
+      <div key={0} data-testid={"tab-views-0"}>
+        {"Home tab"}
+      </div>,
+      <div key={1} data-testid={"tab-views-1"}>
+        {"Following tab"}
+      </div>,
+      <div key={2} data-testid={"tab-views-2"}>
+        {"Disabled tab"}
+      </div>
+    ],
+    testid: "uncontrolled-tab",
+    initialActiveTabIndex: 1,
+    onTabChange: jest.fn()
+  };
+
+  it("should render correctly", () => {
+    render(<Tab {...defaultControlledTabProps} />);
+  });
+
+  it("should pass a11y test", async () => {
+    const {container} = render(<Tab {...defaultControlledTabProps} />);
+
+    await testA11y(container);
   });
 });
