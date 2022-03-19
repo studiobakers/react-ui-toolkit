ed54d2dd1d8b8f79bd8359f68bca421d10a4904a
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2022-03-19 15:35:46 +0300

    fix(select/trigger): Improve props, add todo comment

--- src/select/trigger/SelectTrigger.tsx
+++ src/select/trigger/SelectTrigger.tsx
@@ -6,22 +6,33 @@ import useSelectContext from "../util/hook/useSelectContext";
 
 import "./_select-trigger.scss";
 
-type SelectTriggerProps = Omit<ButtonProps, "onClick">;
+type SelectTriggerProps = Omit<ButtonProps, "type">;
 
-function SelectTrigger({customClassName, children, ...otherProps}: SelectTriggerProps) {
+function SelectTrigger({
+  customClassName,
+  children,
+  onClick,
+  ...otherProps
+}: SelectTriggerProps) {
   const {dispatchSelectStateAction} = useSelectContext();
 
+  //  TODO: Improve focus handling (automatically focus when menu is closed)
   return (
     <Button
       customClassName={classNames("select-trigger", customClassName)}
-      onClick={handleToggleMenuVisibility}
+      type={"button"}
+      onClick={handleClick}
       {...otherProps}>
       {children}
     </Button>
   );
 
-  function handleToggleMenuVisibility() {
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
     dispatchSelectStateAction({type: "TOGGLE_MENU_VISIBILITY"});
+
+    if (onClick) {
+      onClick(event);
+    }
   }
 }
 
